7863f2a86097f2389d3ea2ab1f466d52cbdd61aa
zhongkunyu <enlys1306@gmail.com>
2022-07-10 19:41:34 +0800

    Default Changelist

--- demomp/src/main/java/com/example/controller/BookController.java
+++ demomp/src/main/java/com/example/controller/BookController.java
@@ -7,12 +7,17 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.common.CommonResult;
 import com.example.domain.Book;
 import com.example.service.IBookService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -25,6 +30,8 @@ public class BookController {
     this.bookService = bookService;
   }
 
+//  public static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
   @GetMapping
   public CommonResult<List<Book>> getList() throws Exception {
 //    if (true) {
@@ -36,6 +43,7 @@ public class BookController {
 
   @PostMapping
   public CommonResult<Boolean> save(@RequestBody Book book) {
+    log.info("save start:"+ book.toString());
 //    Book book = new Book();
     Boolean flag =  bookService.save(book);
     return  CommonResult.success(flag);
